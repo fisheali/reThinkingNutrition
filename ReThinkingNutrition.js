9698c71645eb4837b6d207f062db2e3997556e8f
Calvin Todd <CSTodd88@gmail.com>
2021-07-14 18:37:02 -0700

    Removed extra express()

--- ReThinkingNutrition.js
+++ ReThinkingNutrition.js
@@ -4,9 +4,6 @@ var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
-//Create express instance
-var app = express();
-
 //Serves images and CSS
 app.use(express.static('views/images'));
 app.use(express.static('public'));
