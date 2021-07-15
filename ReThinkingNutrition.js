803299f046954f45b3c4e96f1d61541834ac33a5
Calvin Todd <CSTodd88@gmail.com>
2021-07-14 21:48:06 -0700

    Port update

--- ReThinkingNutrition.js
+++ ReThinkingNutrition.js
@@ -1,17 +1,20 @@
 //Load Modules
 var express = require('express');
-var exphbs  = require('express-handlebars');
 
+//Load Express and Express Handlebars
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
-//Create express instance
+//Create static file references
 app.use(express.static('views/images'));
 app.use(express.static('public'));
 
+//Set up view engine
 app.engine('handlebars', exphbs());
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
+app.set('port', 42069);
+
 
 //Serve webpages
 app.get('/', function (req, res) {
