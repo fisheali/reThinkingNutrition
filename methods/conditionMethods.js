6d85a0120373259941f67ba008b1acde12b51be5
fisheali <67443357+fisheali@users.noreply.github.com>
2021-07-27 01:24:04 -0500

    Update conditionMethods.js
    Revised view, add, delete conditions methods to communicate with database.
    Removed updateCondition since conditions only have one attribute.

--- methods/conditionMethods.js
+++ methods/conditionMethods.js
@@ -1,120 +1,47 @@
-function addCondition(data) {
+function addCondition(data, pool, res) {
+
+  pool.query(
+    "INSERT INTO Conditions (condition_name) \
+    VALUES (?)", Object.values(data))
+    .then( confirmation => {
+      console.log(confirmation);      
+      return true;                                 //Confirmation console logginf for debug
+    })
+    .catch( err => {                                                   //Error Catching
+      console.log("FAILED: Add Condition failed with error: " + err);
+      return false;
+    });
+};
+
+function getConditions(data, pool, res) {
   //log data for debug
+  console.log('inside viewAllConditions method');
   console.log(data);
-  title = "Add Condition";
+  pool.query(
+    "SELECT * from Conditions")
+    .then( response => {
+      console.log(response);    //Confirmation console logging for debug
+      let returnData = {};
+      let list = [];
+      for(var i=0; i<response.length; i++)
+      {
+        list.push(response[i]);
+      }
+      returnData.response = list;
+      res.render('viewallconditions', returnData);                                    
+    })
+    .catch( err => {                                                   //Error Catching
+      console.log("FAILED: Add Condition failed with error: " + err);
+    });
 
-  //Database call logic
-
-  //Logic to check if there are multiple records returned
-
-  //placeholder data
-  sql_data =
-    [
-      {"Condition Name": "Dancer's Hip"}
-    ]
-//Potential Data Format Method
-  val = []
-  for (i = 0; i < sql_data.length; i++) {
-    val.push({value : Object.values(sql_data[i])});
-  }
-
-  data = {keys : Object.keys(sql_data[0]), title : title, values: val};
-
-
-  //Return [bool for success, bool for multiple records, data]
-  return [true, data]
-  //Database Call logic
+  pool.release;
 
   //Return failure or success
   return true;
-};
-
-function updateCondition(data) {
-  //log data fro debug
-  console.log(data);
-  title = "Update Condition";
-
-  //Database call logic
-
-  //Logic to check if there are multiple records returned
-
-  //placeholder data
-  sql_data =
-    [
-      {"Condition Name": "Dancer's Leg"}
-    ];
-
-//Potential Data Format Method
-  val = []
-  for (i = 0; i < sql_data.length; i++) {
-    val.push({value : Object.values(sql_data[i])});
-  }
-
-  data = {keys : Object.keys(sql_data[0]), title : title, values: val};
-
-
-  //Return [bool for success, bool for multiple records, data]
-  return [true, true, data]
-};
-
-function removeCondition(data) {
-  //log data fro debug
-  console.log(data);
-  title = "Remove Condition";
-
-  //Database call logic
-
-  //Logic to check if there are multiple records returned
-
-  //placeholder data
-  sql_data =
-    [
-      {"Condition": "Dancer's Toe"}
-    ];
-
-//Potential Data Format Method
-  val = []
-  for (i = 0; i < sql_data.length; i++) {
-    val.push({value : Object.values(sql_data[i])});
-  }
-
-  data = {keys : Object.keys(sql_data[0]), title : title, values: val};
-
-
-  //Return [bool for success, bool for multiple records, data]
-  return [true, true, data]
-};
-
-function getConditions(data) {
-  //log data for debug
-  console.log(data);
-  title = "Conditions";
-
-  //Database call logic
-
-  //Logic to check if there are multiple records returned
-
-  //placeholder data
-  sql_data =
-    [
-      {"Condition": "Dancer's Leg"},
-      {"Condition": "Dancer's Brain"},
-      {"Condition": "No Blood Pressure"}
-    ]
-//Potential Data Format Method
-  val = []
-  for (i = 0; i < sql_data.length; i++) {
-    val.push({value : Object.values(sql_data[i])});
-  }
-
-  data = {keys : Object.keys(sql_data[0]), title : title, values: val};
-
-
-  //Return [bool for success, bool for multiple records, data]
-  return [true, data]
+  
 };
 
 exports.addCondition = addCondition;
 exports.updateCondition = updateCondition;
-exports.removeCondition = removeCondition;
+exports.deleteConditionFromTable = deleteConditionFromTable;
 exports.getConditions = getConditions;
